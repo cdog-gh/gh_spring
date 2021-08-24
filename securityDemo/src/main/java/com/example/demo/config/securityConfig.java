34f867ad9a0d831c9aacd851698398c0d072c060
chogahui <chogahui05@naver.com>
2021-08-24 15:44:22 +0900

    in memory Authentication setting add and authenticate required api add

--- securityDemo/src/main/java/com/example/demo/config/securityConfig.java
+++ securityDemo/src/main/java/com/example/demo/config/securityConfig.java
@@ -1,6 +1,8 @@
 package com.example.demo.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,18 +10,32 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @EnableWebSecurity
 @Configuration
-
 //to use @preAuthorize, @postAuthorized
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class securityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("user")
+                .password("{noop}test")
+                .roles("USER");
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}test2")
+                .roles("ADMIN");
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        //http configuration init
-        super.configure(http);
-
-        // csrf disable
-        // if csrf enable, (post, delete ... )
-        // 403 forbidden without csrf token.
-        http.csrf().disable();
+        http.httpBasic().and().authorizeRequests()
+                /*
+                /test/** : 인증 되어야 함.
+                /user/** : 누구나 접속할 수 있음. (permitAll)
+                 */
+                .antMatchers("/test/**").authenticated()
+                .antMatchers("/user/**").permitAll()
+                .and().formLogin() //form login 활성화
+                .and().csrf().disable(); // csrf disable
     }
 }
